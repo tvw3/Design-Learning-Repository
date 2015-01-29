9346db3606028f56db7f32244dcd9048ca1ed8b8
Thomas Wilkins <thomaswilkins@Thomass-MacBook-Pro.local>
2015-01-29 11:17:59 -0700

    Added course models

--- course/models.py
+++ course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Course(models.Model):
+    #ie cs249, cs386
+    course_ID = models.CharField(max_length=20)
+    #Name of the instructor
+    instructor = models.ForeignKey(User)
+    institution = models.ForeignKey(Institution)
+    date_created = models.DateTimeField('Date Created',auto_now_add=True)
+    date_ending = models.DateTimeField(null = True)
+    
+    #Current course based off semester system ~5months
+    def is_current(self):
+        return timezone.now() <= self.date_ending
+    
+    def __str__(self):
+        return "%s: %s - Instructor: %s %s" % (self.course_ID,self.institution,self.instructor.first_name,self.instructor.last_name)
