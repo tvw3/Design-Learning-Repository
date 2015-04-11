2dfbf35ba8de687354893de1b5e9f8c3b1064c03
Thomas <tvw3@nau.edu>
2015-04-10 17:31:09 -0700

    Clean up

--- static/js/password.js
+++ static/js/password.js
@@ -1,25 +1,3 @@
-
-//This function enables all of the fields for creating a new institution
-function enableSchoolFields(chkbx) {
-    //if the checkbox is checked, make all institution fields required
-    //and make them visible
-    if (chkbx.checked) {
-        var fs = document.getElementById('schoolfields');
-        var fields = fs.getElementsByTagName('input');
-        for (var i = fields.length; i-->0;) {
-        fields[i].required=true;
-        }
-        fs.disabled = false;
-    //otherwise make all fields not required and disablethem
-    } else {
-        var fs = document.getElementById('schoolfields');
-        var fields = fs.getElementsByTagName('input');
-        for (var i = fields.length; i-->0;) {
-         fields[i].required=false;
-        }
-        fs.disabled = true;
-    }
-}
   //simple passwordchecker to make sure that passwords match
 function checkPassword() {
     var pw1 = document.getElementById("pw1");
